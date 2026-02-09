39c14f10a0ae89749ed471aba7b65b7105ab7c02
cnc-lasercraft <96776729+cnc-lasercraft@users.noreply.github.com>
2026-02-09 09:00:37 +0100

    Add constants for device saver configuration

--- custom_components/device_saver/const.py
+++ custom_components/device_saver/const.py
@@ -1,1 +1,14 @@
 
+DOMAIN = "device_saver"
+
+CONF_DEVICES = "devices"
+CONF_TIMEOUT_MIN = "timeout_minutes"
+CONF_NOTIFY_SERVICE = "notify_service"
+CONF_NOTIFY_RECOVERED = "notify_recovered"
+
+DEFAULT_TIMEOUT_MIN = 10
+DEFAULT_NOTIFY_RECOVERED = True
+
+PLATFORMS = ["binary_sensor", "sensor"]
+
+STATE_BAD = {"unavailable", "unknown"}
