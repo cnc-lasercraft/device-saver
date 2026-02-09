696bbac4eddbc3319c58719d6e4c94d28e81675d
cnc-lasercraft <96776729+cnc-lasercraft@users.noreply.github.com>
2026-02-09 09:48:36 +0100

    Update const.py

--- custom_components/device_saver/const.py
+++ custom_components/device_saver/const.py
@@ -4,14 +4,16 @@ CONF_DEVICES_CRIT = "devices_critical"
 CONF_DEVICES_NORM = "devices_normal"
 CONF_DEVICES_SLOW = "devices_slow"
 
-# fixed tiers (can be made configurable later)
-TIMEOUT_CRIT = 1          # minutes
-TIMEOUT_NORM = 180        # 3 hours
-TIMEOUT_SLOW = 10080      # 1 week
+CONF_TIMEOUT_CRIT_MIN = "timeout_critical_minutes"
+CONF_TIMEOUT_NORM_MIN = "timeout_normal_minutes"
+CONF_TIMEOUT_SLOW_MIN = "timeout_slow_minutes"
+
+DEFAULT_TIMEOUT_CRIT_MIN = 1          # 1 minute
+DEFAULT_TIMEOUT_NORM_MIN = 180        # 3 hours
+DEFAULT_TIMEOUT_SLOW_MIN = 10080      # 1 week
 
 CONF_NOTIFY_SERVICE = "notify_service"
 CONF_NOTIFY_RECOVERED = "notify_recovered"
-
 DEFAULT_NOTIFY_RECOVERED = True
 
 PLATFORMS = ["binary_sensor", "sensor"]
