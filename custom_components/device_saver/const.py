261e9a3460e3847b0a53f9c41c5ff582753a1859
cnc-lasercraft <96776729+cnc-lasercraft@users.noreply.github.com>
2026-02-09 09:15:03 +0100

    Add device categories and timeout configurations
    Added new device categories and timeout constants.

--- custom_components/device_saver/const.py
+++ custom_components/device_saver/const.py
@@ -1,12 +1,17 @@
-
 DOMAIN = "device_saver"
 
-CONF_DEVICES = "devices"
-CONF_TIMEOUT_MIN = "timeout_minutes"
+CONF_DEVICES_CRIT = "devices_critical"
+CONF_DEVICES_NORM = "devices_normal"
+CONF_DEVICES_SLOW = "devices_slow"
+
+# fixed tiers (can be made configurable later)
+TIMEOUT_CRIT = 1          # minutes
+TIMEOUT_NORM = 180        # 3 hours
+TIMEOUT_SLOW = 10080      # 1 week
+
 CONF_NOTIFY_SERVICE = "notify_service"
 CONF_NOTIFY_RECOVERED = "notify_recovered"
 
-DEFAULT_TIMEOUT_MIN = 10
 DEFAULT_NOTIFY_RECOVERED = True
 
 PLATFORMS = ["binary_sensor", "sensor"]
